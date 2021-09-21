9186f2243282c2fc9b69d521056c2554b0643083
boziyoung <boziyoung@gmail.com>
2021-09-21 20:39:19 +0800

    'eorry_code'

--- article/views.py
+++ article/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import ArticleColumn
+from .models import ArticleColumn, ArticlePost
 # 9月13日0.2版本
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-from .forms import ArticleColumnForm
+from .forms import ArticleColumnForm, ArticlePostForm
 # 9月14日0.3版本
 from django.views.decorators.http import require_POST
 # Create your views here.
@@ -71,3 +71,26 @@ def del_article_column(request):
         return HttpResponse("1")
     except:
         return HttpResponse("2")
+
+
+@login_required(login_url='/account/login')
+@csrf_exempt
+def article_post(request):
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            cd = article_post_form.cleaned_data
+            try:
+                new_article = article_post_form.save(commit=False)
+                new_article.author = request.user
+                new_article.column = request.user.article_column.get(id=request.POST['column_id'])
+                new_article.save()
+                return HttpResponse("1")
+            except:
+                return HttpResponse("2")
+        else:
+            return HttpResponse("3")
+    else:
+        article_post_form = ArticlePostForm()
+        article_columns = request.user.article_column.all()
+        return render(request, "article/column/article_post.html", {"article_post_form": article_post_form, "article_columns":article_columns})
